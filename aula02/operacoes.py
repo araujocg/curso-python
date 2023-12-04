70f88a08ba83d1466c08c00dd73535bc9603860e
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2023-12-03 20:34:31 -0400

    aulas novas 03/12/13

---
+++ aula02/operacoes.py
@@ -0,0 +1,12 @@
+faturamento = 5000
+custo = 2000
+numeroAleatorio = 3.333
+
+print("Faturamento:", faturamento)  #Faturamento: 5000
+print("Custo:", custo) #Custo: 2000
+print("Lucro:", faturamento - custo) #Lucro: 3000
+print(round(numeroAleatorio)) #3
+
+visual = 592323424
+visual = 592_323_424 # Edição apenas visual com o _
+print(visual) # 592323424
---
+++ aula03/texto.py
@@ -0,0 +1,10 @@
+faturamento = 1000
+custo = 700
+lucro = faturamento- custo
+
+print(f"Faturamento:{faturamento}, Custo:{custo}, lucro:{lucro}")
+print("Faturamento:",faturamento,", Custo:",custo,", lucro:",lucro)
+print("Faturamento: " + str(faturamento)) #Precisa do str() pq só da pra concatenar Strings
+
+email = "Draki.araujo@gmail.com"
+
