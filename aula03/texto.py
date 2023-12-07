74610ead2b9146fd4f7dee2e549067b3f6ed0edd
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2023-12-06 23:41:42 -0400

    aulas novas 06/12/23

--- aula03/texto.py
+++ aula03/texto.py
@@ -6,5 +6,7 @@ print(f"Faturamento:{faturamento}, Custo:{custo}, lucro:{lucro}")
 print("Faturamento:",faturamento,", Custo:",custo,", lucro:",lucro)
 print("Faturamento: " + str(faturamento)) #Precisa do str() pq só da pra concatenar Strings
 
-email = "Draki.araujo@gmail.com"
-
+email = "Draki.araujo@gmail.com" # diferente um do outro
+email = "DRAKI.ARAUJO@gmail.com" #
+print(email.lower()) # draki.araujo@gmail.com
+print(email.upper()) #DRAKI.ARAUJO@GMAIL.COM
