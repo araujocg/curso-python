4fbb7327c2a03935a6ce96ede11ceed3705a202a
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2023-12-08 21:30:33 -0400

    aulas novas 08/12/23

--- aula03/texto.py
+++ aula03/texto.py
@@ -9,4 +9,6 @@ print("Faturamento: " + str(faturamento)) #Precisa do str() pq só da pra concat
 email = "Draki.araujo@gmail.com" # diferente um do outro
 email = "DRAKI.ARAUJO@gmail.com" #
 print(email.lower()) # draki.araujo@gmail.com
-print(email.upper()) #DRAKI.ARAUJO@GMAIL.COM
+print(email.upper()) # DRAKI.ARAUJO@GMAIL.COM
+print(email.find("@")) # -1 se ele n encontrar o "elemento"
+print(email[12:]) # @gmail.com
