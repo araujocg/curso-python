f43aacd6525ef084b822b6507d347632d3715d0f
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2023-12-10 22:39:45 -0400

    aulas novas 10/12/23

--- aula03/texto.py
+++ aula03/texto.py
@@ -6,10 +6,14 @@ print(f"Faturamento:{faturamento}, Custo:{custo}, lucro:{lucro}")
 print("Faturamento:",faturamento,", Custo:",custo,", lucro:",lucro)
 print("Faturamento: " + str(faturamento)) #Precisa do str() pq só da pra concatenar Strings
 
-email = "Draki.araujo@gmail.com" # diferente um do outro
 email = "DRAKI.ARAUJO@gmail.com" #
+email = "Draki.araujo@gmail.com" # diferente um do outro por causa das letras maiúsculas e minúsculas
+
 print(email.lower()) # draki.araujo@gmail.com
 print(email.upper()) # DRAKI.ARAUJO@GMAIL.COM
 print(email.find("@")) # -1 se ele n encontrar o "elemento"
 print(email[12:]) # @gmail.com
 print(len(email)) # 22
+
+email_trocado = email.replace("gmail.com", "ababue")
+print(email_trocado) # Draki.araujo@ababue
