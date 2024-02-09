96d97c3396a7816516fed38f0903b1b2094b6e9d
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2024-02-09 14:21:17 -0400

    aulas novas 09/02/24

--- python/aula01/main04.py
+++ python/aula01/main04.py
@@ -1,5 +1,5 @@
 num1 = int(input("Primeiro numero: "))
 num2 = int(input("Segundo: "))
-soma = num1 + num2
+soma = num1 // num2
 print(f"a soma entre {num1} e {num2} é : {soma}")
 # Só pra n zuar
--- python/aula03/main01.py
+++ python/aula03/main01.py
@@ -6,4 +6,4 @@ print(f'é alfabetico? {a.isalpha()}')
 print(f'é alphanumérico? {a.isalnum()}')
 print(f'está em maiúsculas? {a.isupper()}')
 print(f'está em minúsculas? {a.islower()}')
-print(f'está capitalizada? {a.istitle()}') # AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+print(f'está capitalizada? {a.istitle()}') 
