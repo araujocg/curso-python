6e3d3d44ddf7218a8539adfc51c7b78dd16fc665
Guilherme Araújo Magalhães <136641459+araujocg@users.noreply.github.com>
2024-04-03 13:08:14 -0400

    aulas novas 03/04/24

---
+++ python/aula08/main.py
@@ -0,0 +1,24 @@
+# import math
+# from math import sqrt
+
+# num = int(input("Digite um numero:"))
+
+# # rt = math.sqrt(num)
+# rt = sqrt(num)
+
+# print(int(rt))
+
+###########################
+
+# import random
+
+# num = random.randint(1,10)
+
+# print(num)
+
+###########################
+
+# import emoji 
+
+# print(emoji)
+
